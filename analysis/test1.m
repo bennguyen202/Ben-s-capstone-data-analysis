231957ae1fe92901fd4cc2f4de2c95ca779ba1a4
bennguyen202 <btn241@nyu.edu>
2024-09-07 23:20:48 +0400

    added new t test
    t test for individual condition between two subjects added at the bottom

--- analysis/test1.m
+++ analysis/test1.m
@@ -17,12 +17,12 @@ end
 yl = [-0.3 1.1]; % set y limit
 
 
-x = [VRDATA.M_004_UC6.list.TrialNumber]; % extract trial number and logmar score
-y = [VRDATA.M_004_UC6.list.LogMAR];
+x = [VRDATA.abraiz_fast_C6.list.TrialNumber]; % extract trial number and logmar score
+y = [VRDATA.abraiz_fast_C6.list.LogMAR];
 
-c1 = [VRDATA.M_004_UC6.list.EyeCondition] == "Both_Eyes"; % extract eye condition
-c2 = [VRDATA.M_004_UC6.list.EyeCondition] == "Right_Eye";
-c3 = [VRDATA.M_004_UC6.list.EyeCondition] == "Left_Eye";
+c1 = [VRDATA.abraiz_fast_C6.list.EyeCondition] == "Both_Eyes"; % extract eye condition
+c2 = [VRDATA.abraiz_fast_C6.list.EyeCondition] == "Right_Eye";
+c3 = [VRDATA.abraiz_fast_C6.list.EyeCondition] == "Left_Eye";
 
 x1 = x(c1); % match logmar score to condition
 y1 = y(c1);
@@ -36,9 +36,9 @@ x3 = x(c3);
 y3 = y(c3);
 yy3 = y3(end-20:end);
 
-real_both = 0.82; % real logmar score
-real_right = 0.92;
-real_left = 0.86;
+real_both = -0.04; % real logmar score
+real_right = 0.08;
+real_left = 0.02;
 
 a1 = mean(y1(end-20:end)); % mean of VR logmar score (average last 20 trials)
 a2 = mean(y2(end-20:end));
@@ -129,34 +129,83 @@ annotation('textbox',[1 .4 .5 .1], ...
 % Adjust the figure's position to make room for the ylabel
 fig.Position(3) = fig.Position(3) + .1; % does not currently work
 
-%% stats stuff
-% input real and average vr data manually
+% %% stats stuff
+% % input real and average vr data manually
 % s.realuc = [0.82 0.86 0.94 0.9 1 0.92 0.62 0.54 0.8 0.96 0.9 0.96 0.82 0.86 0.92];
-s.realall = [0.82 0.86 0.94 0 0 0.02 -0.1 0.04 0.02 0.9 1 0.92 -0.04 0.08 0.02 0.1 0 0.1 -0.1 -0.06 -0.06 -0.2 -0.14 0.24 -0.02 0.16 -0.1 0.62 0.54 0.8 0.96 0.9 0.96 0.82 0.86 0.92 -0.06 -0.02 0.06];
+% s.realall = [0.82 0.86 0.94 0 0 0.02 -0.1 0.04 0.02 0.9 1 0.92 -0.04 0.08 0.02 0.1 0 0.1 -0.1 -0.06 -0.06 -0.2 -0.14 0.24 -0.02 0.16 -0.1 0.62 0.54 0.8 0.96 0.9 0.96 0.82 0.86 0.92 -0.06 -0.02 0.06];
 % s.vruc = [0.665497261918232 0.750300732266265 0.763311606785522 0.878368248556045 1.044014133751761 0.848559040157549 0.490120243926443 0.528723041978598 0.772323108065766 0.891009414178191 0.874557659847996 0.895669638661807 0.583411264571096 0.634869161284951 0.839547669133916];
-s.vrall = [0.665497261918232 0.750300732266265 0.763311606785522 0.511119184059796 0.676288799375090 0.676288799375090 0.461175974933904 0.448014772056905 0.508994051604219 0.878368248556045 1.044014133751761 0.848559040157549 0.344963558816919 0.410338024033777 0.409680505491749 0.452044167722371 0.460406194148358 0.510208969938377 0.438672456198216 0.506219309296467 0.501491072476946 0.461504781057158 0.472095399703105 0.622153089897715 0.548618153749264 0.459622033169327 0.454192000057477 0.490120243926443 0.528723041978598 0.772323108065766 0.891009414178191 0.874557659847996 0.895669638661807 0.583411264571096 0.634869161284951 0.839547669133916 0.404905950477337 0.439625371447288 0.436903653647372];
-Rall = corrcoef(s.realall,s.vrall); % calculate correlation between real and vr
-rsq = Rall(2,1)^2
+% s.vrall = [0.665497261918232 0.750300732266265 0.763311606785522 0.511119184059796 0.676288799375090 0.676288799375090 0.461175974933904 0.448014772056905 0.508994051604219 0.878368248556045 1.044014133751761 0.848559040157549 0.344963558816919 0.410338024033777 0.409680505491749 0.452044167722371 0.460406194148358 0.510208969938377 0.438672456198216 0.506219309296467 0.501491072476946 0.461504781057158 0.472095399703105 0.622153089897715 0.548618153749264 0.459622033169327 0.454192000057477 0.490120243926443 0.528723041978598 0.772323108065766 0.891009414178191 0.874557659847996 0.895669638661807 0.583411264571096 0.634869161284951 0.839547669133916 0.404905950477337 0.439625371447288 0.436903653647372];
+% Rall = corrcoef(s.realall,s.vrall); % calculate correlation between real and vr
+% rsq = Rall(2,1)^2
+% ruc = corrcoef(s.realuc,s.vruc)
+% rsq2 = ruc(2,1)^2
+% 
+% figure % scatter real and vr results (all)
+% scatter(s.realall,s.vrall,'filled')
+% xlabel('Eyechart acuity (logMAR)')
+% ylabel('VR acuity (logMAR)')
+% xlim([-0.3 1.1])
+% ylim([-0.3 1.1])
+% line([-.3 1.1], [-.3 1.1]) % identity (perfect performance)
+% % lsline
+% 
+% % The resolution of the Quest 2 is 20 pixels/degree
+% % so, 10 cycles/degree, or log10(30/cycpdeg) logMar
+% yline(log10(30/10),'-.g','LineWidth',1) % Quest 2
+% yline(log10(30/12.5),'--r','LineWidth',1) % Quest 3
+% legend('data','perfect performance', 'quest 2 limit','quest 3 limit','Location', 'northeastoutside')
+% 
+% 
+% figure % scatter real and vr results (uc)
+% scatter(s.realuc,s.vruc,'filled')
+% xlabel('Eyechart acuity (logMAR)')
+% ylabel('VR acuity (logMAR)')
+% xlim([-0.3 1.1])
+% ylim([-0.3 1.1])
+% line([-.3 1.1], [-.3 1.1]) % identity (perfect performance)
+% lsline
+% 
+% 
+% 
+% legend('data','perfect performance','least square line','Location', 'northeastoutside')
 
-% xx = s.vrall' testing confidence interval
-% xx1 = xx & xx < 0.5
-% xx2 = xx(xx1)
-% ci = fitdist(xx2, 'Normal');
+%% t test
+su1_x = [VRDATA.AB_009_C6.list.TrialNumber]; % extract trial number and logmar score for subject 1
+su1_y = [VRDATA.AB_009_C6.list.LogMAR];
 
-figure % scatter real and vr results (all)
-scatter(s.realall,s.vrall,'filled')
-xlabel('Eyechart acuity (logMAR)')
-ylabel('VR acuity (logMAR)')
-xlim([-0.3 1.1])
-ylim([-0.3 1.1])
-line([-.3 1.1], [-.3 1.1]) % identity (perfect performance)
-lsline
+su1_c1 = [VRDATA.AB_009_C6.list.EyeCondition] == "Both_Eyes"; % extract eye condition
+su1_c2 = [VRDATA.AB_009_C6.list.EyeCondition] == "Right_Eye";
+su1_c3 = [VRDATA.AB_009_C6.list.EyeCondition] == "Left_Eye";
 
-% The resolution of the Quest 2 is 20 pixels/degree
-% so, 10 cycles/degree, or log10(30/cycpdeg) logMar
-yline(log10(30/10),'LineWidth',1) % Quest 2
-yline(log10(30/12.5),'r','LineWidth',1) % Quest 3
-legend('data','perfect performance', 'least square line', 'quest 2 limit','quest 3 limit','Location', 'northeastoutside')
+su1_x1 = su1_x(c1); % match logmar score to condition
+su1_b = su1_y(c1);
 
 
+su1_x2 = su1_x(c2);
+su1_r = su1_y(c2);
 
+
+su1_x3 = su1_x(c3);
+su1_l = su1_y(c3);
+
+
+su2_x = [VRDATA.abraiz_fast_C6.list.TrialNumber]; % extract trial number and logmar score for subject 2
+su2_y = [VRDATA.abraiz_fast_C6.list.LogMAR];
+
+su2_c1 = [VRDATA.abraiz_fast_C6.list.EyeCondition] == "Both_Eyes"; % extract eye condition
+su2_c2 = [VRDATA.abraiz_fast_C6.list.EyeCondition] == "Right_Eye";
+su2_c3 = [VRDATA.abraiz_fast_C6.list.EyeCondition] == "Left_Eye";
+
+su2_x1 = su2_x(c1); % match logmar score to condition
+su2_b = su2_y(c1);
+
+
+su2_x2 = su2_x(c2);
+su2_r = su2_y(c2);
+
+
+su2_x3 = su2_x(c3);
+su2_l = su2_y(c3);
+
+
+[h,p] = ttest2(su1_b,su2_b) % two sample t test for individual condition
